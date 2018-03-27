12102f6c05c03b60c303ab55858ffd9a542fa950
Peter Selby <ps664@cornell.edu>
2018-03-27 13:37:18 -0400

    update script

--- Scripts/combineSwagger.py
+++ Scripts/combineSwagger.py
@@ -7,9 +7,14 @@ import sys
 rootPath = '.'
 if len(sys.argv) > 1 :
 	rootPath = sys.argv[1];
+
+swagger = '2.0'
+host = 'https://test-server.brapi.org/'
+basePath = '/brapi/v1'
 info = {}
 paths = {}
 defin = {}
+
 for filename in glob.iglob(rootPath + '/**/*.yaml', recursive=True):
 	print(filename)
 	with open(filename, "r") as stream:
@@ -20,7 +25,13 @@ for filename in glob.iglob(rootPath + '/**/*.yaml', recursive=True):
 			defin.update(fileObj['definitions'])
 		except yaml.YAMLError as exc:
 			print(exc)
-out = {'swagger': '2.0', 'info': info, 'paths': paths, 'definitions' : defin}
+
+out = {'swagger': swagger,
+	'host': host,
+	'basePath': basePath,
+	'info': info,
+	'paths': paths,
+	'definitions' : defin}
 
 with open('out.yaml', 'w') as outfile:
 	yaml.dump(out, outfile, default_flow_style=False)
