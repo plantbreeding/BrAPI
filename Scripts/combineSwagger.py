463709044d8033c411f7ba51c92b73f0c68f1b2d
Peter Selby <ps664@cornell.edu>
2018-03-27 15:43:53 -0400

    update Tags

--- Scripts/combineSwagger.py
+++ Scripts/combineSwagger.py
@@ -5,13 +5,12 @@ import glob
 import sys
 
 rootPath = '.'
+metaFilePath = './swaggerMetaData.yaml'
 if len(sys.argv) > 1 :
 	rootPath = sys.argv[1];
+if len(sys.argv) > 2 :
+	metaFilePath = sys.argv[2];
 
-swagger = '2.0'
-host = 'https://test-server.brapi.org/'
-basePath = '/brapi/v1'
-info = {}
 paths = {}
 defin = {}
 
@@ -20,18 +19,19 @@ for filename in glob.iglob(rootPath + '/**/*.yaml', recursive=True):
 	with open(filename, "r") as stream:
 		try:
 			fileObj = yaml.load(stream)
-			info.update(fileObj['info'])
 			paths.update(fileObj['paths'])
 			defin.update(fileObj['definitions'])
 		except yaml.YAMLError as exc:
 			print(exc)
 
-out = {'swagger': swagger,
-	'host': host,
-	'basePath': basePath,
-	'info': info,
-	'paths': paths,
-	'definitions' : defin}
+out = {}
+with open(metaFilePath, "r") as metaFile:
+	try:
+		out = yaml.load(metaFile)
+	except yaml.YAMLError as exc:
+		print(exc)
+		
+out.update({'paths': paths, 'definitions' : defin})
 
 with open('out.yaml', 'w') as outfile:
 	yaml.dump(out, outfile, default_flow_style=False)
