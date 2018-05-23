28dab1ed0e01bac33b614bff9b03d6f366b971a2
Peter Selby <ps664@cornell.edu>
2018-05-23 16:08:57 -0400

    generate README files script

--- Scripts/generateReadMe.py
+++ Scripts/generateReadMe.py
@@ -5,77 +5,95 @@ import glob
 import sys
 import json
 
+
+def buildReadMe(dir):
+	readMeStr = ''
+	for filename in glob.iglob(dir + '/*.yaml', recursive=True):
+		print(filename)
+		fileObj = {}	
+		with open(filename, "r") as stream:
+			try:
+				fileObj = yaml.load(stream)
+				stream.close()
+			except yaml.YAMLError as exc:
+				print(exc)
+		
+		if 'paths' in fileObj:
+			callPath = list(fileObj['paths'].keys())[0]
+			methods = fileObj['paths'][callPath]
+			for methodKey in methods.keys():
+				methodObj = methods[methodKey]
+				
+				desc = ''
+				if 'description' in methodObj:
+					desc = methodObj['description']
+				
+				params = []
+				if 'parameters' in methodObj:
+					params = methodObj['parameters']
+					
+				responses = []
+				if 'responses' in methodObj:
+					responses = methodObj['responses']
+				
+				readMeStr += '\n\n## ' + callPath[1:].capitalize() + ' [' + methodKey.capitalize() + ' /brapi/v1' + callPath
+				for param in params:
+					if param['in'] == 'query' :
+					    readMeStr += '{?' + param['name'] + '}'
+					    
+				readMeStr += ']\n\n' + desc
+				readMeStr += ' \n\n+ Parameters\n'
+				body = ''
+				for param in params:
+					if param['in'] == 'body':
+						body = param['schema']['$ref'][1:] if '$ref' in param['schema'] else json.dumps(param['schema'], indent=4, separators=(',', ': '), default=str)
+					else:
+						readMeStr += '    + ' + param['name'] 
+						
+						if ('required' in param) : 
+							readMeStr += ' (Required, ' if param['required'] else ' (Optional, '
+						else:
+							readMeStr += ' (Optional, '
+						
+						readMeStr += param['type'] + ') ... ' if 'type' in param else ') ... '
+						readMeStr += param['description'] if 'description' in param else ''
+						readMeStr += '\n'
+				
+				if body != '' :
+					readMeStr += ' \n+ Request (application/json)\n' + body
+					
+				
+				readMeStr += '\n\n'
+				for code in responses:
+					for type in responses[code]['examples']:
+						example = responses[code]['examples'][type]
+						readMeStr += '+ Response ' + code + ' (' + type + ')\n```\n'
+						readMeStr += json.dumps(example, indent=4, separators=(',', ': '), default=str)
+						readMeStr += '\n```'
+	return readMeStr
+	
+
 rootPath = '.'
+specificPath = ''
 if len(sys.argv) > 1 :
 	rootPath = sys.argv[1];
+if len(sys.argv) > 2 :
+	specificPath = sys.argv[2];
 
-readMeStr = ''
-for filename in glob.iglob(rootPath + '/**/*.yaml', recursive=True):
-	print(filename)
-	fileObj = {}	
-	with open(filename, "r") as stream:
-		try:
-			fileObj = yaml.load(stream)
-			stream.close()
-		except yaml.YAMLError as exc:
-			print(exc)
+if specificPath == '' :
+	for dir in glob.iglob(rootPath + '/**/', recursive=False):
+		print(dir)
+		readMeStr = buildReadMe(dir)
+		#with open(dir + '/README.md.gen', 'w') as outfile:
+		#	outfile.write(readMeStr)
+else:
+	dir = rootPath + specificPath
+	print(dir)
+	readMeStr = buildReadMe(dir)
+	#with open(dir + '/README.md.gen', 'w') as outfile:
+	#	outfile.write(readMeStr)
 	
-	if 'paths' in fileObj:
-		callPath = list(fileObj['paths'].keys())[0]
-		methods = fileObj['paths'][callPath]
-		for methodKey in methods.keys():
-			methodObj = methods[methodKey]
-			
-			desc = ''
-			if 'description' in methodObj:
-				desc = methodObj['description']
-			
-			params = []
-			if 'parameters' in methodObj:
-				params = methodObj['parameters']
-				
-			responses = []
-			if 'responses' in methodObj:
-				responses = methodObj['responses']
-			
-			readMeStr += '\n\n## ' + callPath[1:].capitalize() + ' [' + methodKey.capitalize() + ' /brapi/v1' + callPath
-			for param in params:
-				if param['in'] == 'query' :
-				    readMeStr += '{?' + param['name'] + '}'
-				    
-			readMeStr += ']\n\n' + desc
-			readMeStr += ' \n\n+ Parameters\n'
-			body = ''
-			for param in params:
-				if param['in'] == 'body':
-					body = param['schema']['$ref'][1:]
-				else:
-					readMeStr += '    + ' + param['name'] 
-					
-					if ('required' in param) : 
-						readMeStr += ' (Required, ' if param['required'] else ' (Optional, '
-					else:
-						readMeStr += ' (Optional, '
-					
-					readMeStr += param['type'] + ') ... ' if 'type' in param else ') ... '
-					readMeStr += param['description'] if 'description' in param else ''
-					readMeStr += '\n'
-			
-			if body != '' :
-				readMeStr += ' \n+ Request (application/json)\n' + body
-				
-			
-			readMeStr += '\n\n'
-			for code in responses:
-				for type in responses[code]['examples']:
-					example = responses[code]['examples'][type]
-					readMeStr += '+ Response ' + code + ' (' + type + ')\n```\n'
-					readMeStr += json.dumps(example, indent=4, separators=(',', ': '))
-					readMeStr += '\n```'
+		
 
+#print(readMeStr)
 	
-
-print(readMeStr)
-	
-with open(rootPath + '/README.md.gen', 'w') as outfile:
-	outfile.write(readMeStr)
