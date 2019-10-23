202fe3fbb32847f572920bc2550f925207ed4540
Peter Selby <ps664@cornell.edu>
2019-10-23 13:42:06 -0500

    update publish swaggerhub script

--- Scripts/publishToSwaggerHub.sh
+++ Scripts/publishToSwaggerHub.sh
@@ -2,10 +2,23 @@
 
 # Param 1: compiled OpenAPI file (default "$HOME/brapi_blueprint.apib")
 
-if [ -f "$1" ]; then
-	BRAPI_FILE=${1};
+if [ -z "$1" ]; then
+	echo "usage: 'publishToSwaggerHub <blueprint file> <apiary name> <apiary key>"
+	exit 1
 else
-	BRAPI_FILE=${HOME}/brapi_openapi.yaml;
+	BRAPI_FILE=${1};
+fi
+if [ -z "$2" ]; then
+	echo "usage: 'publishToSwaggerHub <blueprint file> <apiary name> <apiary key>"
+	exit 1
+else
+	APINAME=${2}
+fi
+if [ -z "$3" ]; then
+	echo "usage: 'publishToSwaggerHub <blueprint file> <apiary name> <apiary key>"
+	exit 1
+else
+	SWAGGERHUB_TOKEN=${3}
 fi
 
 # Publish to apiary
@@ -16,7 +29,7 @@ curl -i \
   --header "Authorization:Bearer $SWAGGERHUB_TOKEN" \
   --header "content-type:application/yaml; charset=utf-8" \
   --data-binary @$BRAPI_FILE \
-  https://api.swaggerhub.com/apis/PlantBreedingAPI/BrAPI
+  https://api.swaggerhub.com/apis/PlantBreedingAPI/$APINAME
 
 if [ $? -ne 0 ]; then
 	echo "ERROR: SwaggerHub rejected the file"
