f089ecc1f7bd3fb92a3c6915954bfa35f4a4226d
Peter Selby <ps664@cornell.edu>
2023-06-27 11:05:55 -0400

    auto update

--- Scripts/buildJSONSchema2.py
+++ Scripts/buildJSONSchema2.py
@@ -79,16 +79,17 @@ def addRefs(parent):
 				className = newParent['x-brapi-metadata']['title']
 			for term in parent['properties']:
 				if term.endswith('DbId') and (term.casefold() != (className + 'DbId').casefold()):
+					oldTermDetails = newParent['properties'].pop(term)
 					newTerm = term[:-4]
 					newTermTitle = newTerm[0].upper() + newTerm[1:]
-					oldTermDetails = newParent['properties'].pop(term)
 					if newTerm + 'Name' in newParent['properties']:
 						newParent['properties'].pop(newTerm + 'Name')
 					newParent['properties'][newTerm] = { '$ref': newTermTitle + '.json#/$defs/' + newTermTitle, 'description': oldTermDetails['description'], 'relationshipType': 'EDITME-to-one', 'referencedAttribute': className[0].lower() + className[1:] + 's' }
 				elif term.endswith('DbIds') and (term.casefold() != (className + 'DbIds').casefold()):
-					newTerm = term[:-5]
 					oldTermDetails = newParent['properties'].pop(term)
-					newParent['properties'][newTerm + 's'] = { 'items':{ '$ref': newTerm + '.json#/$defs/' + newTerm }, 'type': 'array', 'description': oldTermDetails['description'], 'relationshipType': 'EDITME-to-many', 'referencedAttribute': className[0].lower() + className[1:] + 's' }
+					newTerm = term[:-5]
+					newTermTitle = newTerm[0].upper() + newTerm[1:]
+					newParent['properties'][newTerm + 's'] = { 'items':{ '$ref': newTermTitle + '.json#/$defs/' + newTermTitle }, 'type': 'array', 'description': oldTermDetails['description'], 'relationshipType': 'EDITME-to-many', 'referencedAttribute': className[0].lower() + className[1:] + 's' }
 	
 	return newParent
 
