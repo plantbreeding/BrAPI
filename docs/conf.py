f16e8c619a2b754849e8df6351701199b38d727d
Peter Selby <32845555+BrapiCoordinatorSelby@users.noreply.github.com>
2024-09-11 16:46:50 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,9 +28,9 @@ author = 'BrAPI Community'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 # import sphinx_rtd_theme
-extensions = [
-    'sphinx_rtd_theme'
-]
+# extensions = [
+#     'sphinx_rtd_theme'
+# ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
