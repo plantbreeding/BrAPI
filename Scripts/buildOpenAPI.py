03af5b8f3f2869c6ec502024f1ece46fd1f30105
Peter Selby <ps664@cornell.edu>
2025-02-24 18:10:39 -0500

    python yaml updates

--- Scripts/buildOpenAPI.py
+++ Scripts/buildOpenAPI.py
@@ -32,7 +32,7 @@ def go(rootPaths, metaFilePath = './swaggerMetaData.yaml'):
     for filename in filenames:
         with open(filename, "r") as stream:
             try:
-                fileObj = yaml.load(stream)
+                fileObj = yaml.safe_load(stream)
                 if 'paths' in fileObj:
                     paths.update(fileObj['paths'])
                 if 'components' in fileObj:
@@ -55,7 +55,7 @@ def go(rootPaths, metaFilePath = './swaggerMetaData.yaml'):
     out = {}
     with open(metaFilePath, "r") as metaFile:
         try:
-            out = yaml.load(metaFile)
+            out = yaml.safe_load(metaFile)
         except yaml.YAMLError as exc:
             print(exc)
             
